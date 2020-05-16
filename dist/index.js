4283395c83496194ce6ca5351617a30180aa9167
Nikita Gimpel <gimpelllll@gmail.com>
2020-05-16 15:22:36 +0300

    Rework full file

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,55 @@
 let uid = 0;
-let workers = [];
+const workers = [];
 
-const hasNativeSupport = !!window.BigInt;
+const SORTED_STUCTURE = {
+    factorial: ['fib_fact_js', 'fib_fact_c', 'fib_fact_cpp'],
+    fibonacci: ['fib_fact_js', 'fib_fact_c', 'fib_fact_cpp'],
+    encrypt: ['RSA_js', 'RSA_c', ''],
+};
 
-const onSubmit = e => {
+function getViewValues(maxColumnLength, ...args) {
+    const viewValues = args.map((el) => {
+        return el.length > maxColumnLength ?
+        el.slice(0, maxColumnLength) + '...' :
+        el;
+    });
+    return viewValues.join(', ');
+}
+
+function parseValues(...args) {
+    return args.map((el) => {
+        try {
+            const parsedValue = JSON.parse(el);
+            switch (typeof parsedValue) {
+                case 'number':
+                    return Math.max(1, parsedValue);
+                case 'object':
+                    return Array.isArray(parsedValue) ? parsedValue : null;
+                default:
+                    return null;
+            }
+        } catch {
+            return null;
+        }
+    })
+
+}
+
+
+function getArguments(functionName, elements) {
+    const argsLength = functionName === 'encrypt' ? 3 : 1;
+
+    return [].slice.call(elements, 0, argsLength).map((el) => el.value);
+}
+
+function getWorkerArguments(args) {
+    const {length} = args;
+    return length > 1 ?
+        [args[0], length, ...args.slice(1)] :
+        [args[0]];
+}
+
+function onSubmit(e) {
     if (e && e.preventDefault) {
         e.preventDefault();
     }
@@ -14,32 +60,38 @@ const onSubmit = e => {
         throw new Error('target is malformed');
     }
 
-    let { elements: { value: { value } }, id: fn } = target;
+    const { elements, id: fn } = target;
+    const args = getArguments(fn, elements);
+    if (!args) {
+        return;
+    }
+    const parsedArgs = parseValues(...args);
     const row = document.createElement('tr');
     const header = document.createElement('th');
 
-    value = Math.max(1, parseInt(value, 10));
-    header.innerText = `${fn}(${value})`;
+    const maxColumnLength = 7;
+    
+    header.innerText = `${fn}(${getViewValues(maxColumnLength, args)})`;
     row.appendChild(header);
 
-    if (!hasNativeSupport) {
+    SORTED_STUCTURE[fn].forEach((workerName) => {
+        const workerObject = workers.find(({name}) => name === workerName);
         const column = document.createElement('td');
-        column.innerText = 'Unsupported';
+
+        if (workerObject) {
+            const id = `${workerName}-${uid}`;
+            column.id = id;
+
+            workerObject.worker.postMessage({
+                id,
+                fn,
+                args: getWorkerArguments(parsedArgs)
+            });
+        } else {
+            column.innerText = 'None';
+        }
+
         row.appendChild(column);
-    }
-
-    workers.forEach(([name, w]) => {
-        const id = `${name}-${uid}`;
-
-        const column = document.createElement('td');
-        column.id = id;
-        row.appendChild(column);
-
-        w.postMessage({
-            id,
-            fn,
-            args: [value]
-        });
     });
 
     const firstRow = document.querySelector('table tr:first-child');
@@ -50,16 +102,9 @@ const onSubmit = e => {
 }
 
 (() => {
-    if (!window.Worker) {
-        return alert('Unfortunately, your browser does not support WebWorkers');
-    }
-
     let initializedWorkers = 0;
 
-    const workerNames = ['gmp', 'ttmath'];
-    if (hasNativeSupport) {
-        workerNames.unshift('native');
-    }
+    const workerNames = ['RSA_c', 'RSA_js', 'fib_fact_c', 'fib_fact_cpp', 'fib_fact_js'];
 
     workerNames.forEach(name => {
         const worker = new Worker(`./workers/${name}.js`);
@@ -70,27 +115,19 @@ const onSubmit = e => {
                 if (initializedWorkers === workers.length) {
                     document.querySelectorAll('form').forEach(f => f.addEventListener('submit', onSubmit));
                     document.querySelectorAll('button').forEach(b => b.disabled = false);
-                    onSubmit({
-                        target: {
-                            id: 'fibonacci',
-                            elements: {
-                                value: { value: 100 },
-                            },
-                        },
-                    });
                 }
                 break;
             case 'execComplete':
                 console.debug(args);
                 const el = document.getElementById(args.id);
                 if (el) {
-                    el.innerText = `${ args.calcTime.toFixed(2) }ms (+ ${ args.toStringTime.toFixed(2) }ms)`;
+                    el.innerText = `${ args.calcTime.toFixed(3) }ms ${ args.toStringTime ? '(+ ' + args.toStringTime.toFixed(3) + ' ms)' : ''}`;
                 }
                 break;
             default:
                 console.error(`Unknown message: '${message}'`);
             }
         };
-        workers.push([name, worker]);
+        workers.push({name, worker});
     });
 })();
